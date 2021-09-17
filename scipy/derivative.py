7a6028d324b1c51a675193b8c9d2c1a85f79007e
Paramesh Chandra <parameshchandra28@gmail.com>
2021-09-17 16:02:54 +0530

    .

---
+++ scipy/derivative.py
@@ -0,0 +1,12 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.misc import derivative
+
+def f(x):
+    return x**2 * np.sin(2*x) *np.exp(-x)
+x = np.linspace(0, 1, 100)
+plt.plot(x, f(x))
+plt.plot(x, derivative(f, x, dx=1e-6))
+plt.plot(x, derivative(f, x, dx=1e-6, n=2))
+plt.grid()
+plt.show()
---
+++ scipy/specialfunction.py
@@ -0,0 +1,13 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.special import legendre
+#x = np.linspace(0, 1, 100)
+#for i in range(1,6):
+#    plt.plot(x, legendre(i)(x))
+#plt.show()
+
+from scipy.special import jv
+x = np.linspace(0, 10, 100)
+for i in range(6):
+    plt.plot(x, jv(i,x))
+plt.show()
