42d2160c3f96065e5f00c2f6f612d550251892ed
Paramesh Chandra <parameshchandra28@gmail.com>
2022-01-26 15:30:57 +0530

    Create Montecarlointcosx.py

---
+++ Numericalmethods/Montecarlointcosx.py
@@ -0,0 +1,27 @@
+from random import random, uniform
+from math import sqrt, cos, pi
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+N = 50000  # number of random points in unit cube
+
+
+def calculatearea(N):
+
+    count = 0  # number of points with in sphere
+    for j in range(N):
+        point = (uniform(0, pi), uniform(-1, 1))
+        # print(point)
+        if cos(point[0]) <= abs(point[1]):
+            if point[1] < 0:
+                count -= 1
+            else:
+                count += 1
+        #print(count)
+        Answer = pi  * float(count) / float(N)
+    return Answer
+
+
+value = calculatearea(N)
+print("The value of =", value)
