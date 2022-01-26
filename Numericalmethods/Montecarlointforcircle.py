f3a946514752ce3dfd3ebc5269f1071135f2a501
Paramesh Chandra <parameshchandra28@gmail.com>
2022-01-26 14:50:06 +0530

    .

--- Numericalmethods/Montecarlointforcircle.py
+++ Numericalmethods/Montecarlointforcircle.py
@@ -4,16 +4,23 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Since the radius is 1, we can leave it out of most calculations.
-N = 10000000  # number of random points in unit cube
+N = 100000  # number of random points in unit cube
 
 
 def calculatepi(N):
-
+    all_x = []
+    all_y = []
+    inside_x = []
+    inside_y = []
     count = 0  # number of points with in sphere
     for j in range(N):
-        point = (random(), random(), random())
-        if point[0] * point[0] + point[1] * point[1] + point[2] * point[2] < 1:
+        point = (random(), random())
+        all_x.append(point[0])
+        all_y.append(point[1])
+        if point[0] * point[0] + point[1] * point[1]  < 1:
             count = count + 1
+            inside_x.append(point[0])
+            inside_y.append(point[1])
     Answer = float(count) / float(N)
     # Make sure to use float, otherwise the answer comes out zero!
     # Also note that in this case the volume of our ” known ” volume(the unit
@@ -22,11 +29,13 @@ def calculatepi(N):
     # print(
     #    "The volume of a hemisphere of radius 1 is ", Answer, "+/−", 4 * sqrt(N) / float(N)
     # )
-    pii = (3 / 4) * Answer * 2
-
+    pii =  Answer 
     # print('the value of the pi is',pii)
-    return pii
+    return pii,all_x,all_y,inside_x,inside_y
 
 
-pii = calculatepi(1000000)
-print("The value of pi =", pii)
+pii = calculatepi(N)
+print("The value of pi =", pii[0])
+plt.scatter(np.array(pii[1]), np.array(pii[2]))
+plt.scatter(np.array(pii[3]), np.array(pii[4]))
+plt.show()
